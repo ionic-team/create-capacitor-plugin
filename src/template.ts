86995f4c119ba047d21c943d886feac8326fcb0f
Fabio Martino <fabiomartino@users.noreply.github.com>
2025-05-14 23:43:46 +0200

    Updated dependencies

--- src/template.ts
+++ src/template.ts
@@ -7,7 +7,7 @@ import type { OptionValues } from './options';
 
 const MUSTACHE_EXTENSION = '.mustache';
 
-export const CAPACITOR_VERSION = '7.0.0';
+export const CAPACITOR_VERSION = '7.2.0';
 
 const TEMPLATE_PATH = resolve(__dirname, '..', 'assets', 'plugin-template.tar.gz');
 
